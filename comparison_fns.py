e88b2b8573b179ab20baf202df6b66399c0fd1de
Colin B. Macdonald <cbm@m.fsf.org>
2025-05-19 15:35:24 -0700

    Small patch using 'with' to avoid needing to close files

--- comparison_fns.py
+++ comparison_fns.py
@@ -3751,9 +3751,8 @@ def determine_branch_compass2(br_th, mt_angle, ref_th):
     return(inward)
 
 #load in lookup table for sampling ellipse distr
-pick_in = open('ellipse_table.pickle','rb')
-lookup_table = pickle.load(pick_in)
-pick_in.close()
+with open('ellipse_table.pickle', 'rb') as pick_in:
+    lookup_table = pickle.load(pick_in)
 
 dy_lookup = lookup_table['dtheta']
 theta_inverse = lookup_table['theta_inverse']

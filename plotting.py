1ad6c077d0ae5a13ccece1ffa70af3d0fcb13f25
Colin B. Macdonald <cbm@m.fsf.org>
2025-05-28 23:33:23 -0700

    non-executable support functions

--- plotting.py
+++ plotting.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Apr 14 11:41:58 2021
--- zippering.py
+++ zippering.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Mon Mar  8 23:09:10 2021
